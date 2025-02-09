308ed5514f7bf01c738997582359b97f9353d8ad
jq058 <jqlau9096@gmail.com>
2025-02-09 23:21:46 +0800

    added comments and best practice

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,12 @@ export default function Home() {
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Welcome to the COVID-19 InfoHub!</h1>
-            <p className={styles.description}>Track the spread of COVID-19 across the world with an interactive heatmap.</p>
+            <p className={styles.description}>Track the spread of COVID-19 across the world with our tools</p>
 
             {/* Navigate to Heatmap */}
             <div className={styles.navContainer}>
                 <Link href="/heatmap">
-                    <button className={styles.button}>Go to Heatmap</button>
+                    <button className={styles.button}>Find out more</button>
                 </Link>
             </div>
         </div>
